670433bfcef4167aabf0329190d6e8a08fa4312c
Luis Fernando <fernandoluis270@gmail.com>
2025-09-04 20:57:10 -0400

    buscando todas as cidades de um determinado estado e removendo as cidades do estado escolhido anteriormente

--- assets/js/services/buscarCidades.js
+++ assets/js/services/buscarCidades.js
@@ -1,12 +1,17 @@
 async function buscarCidades() {
     let estadoId = document.getElementById('estado').value
     let selectCidade = document.getElementById('cidade')
+    
+    selectCidade.innerHTML = ''
+
+    let defaultOption = new Option(`Selecione uma cidade`, ``)
+    selectCidade.add(defaultOption)
 
     try {
         const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoId}/municipios`)
 
         const data = await response.json()
-        
+
         for (const cidade of data) {
             let newOption = new Option(cidade.nome, cidade.id)
             selectCidade.add(newOption)
