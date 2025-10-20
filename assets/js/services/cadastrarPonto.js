7924b0eca156bb7a9a455a3ad946c2b2f24fd655
luis-fernando-job <fernandoluis270@gmail.com>
2025-10-20 16:24:07 -0400

    cadastrando ponto de descarte

--- assets/js/services/cadastrarPonto.js
+++ assets/js/services/cadastrarPonto.js
@@ -1,6 +1,19 @@
 let myForm = document.getElementById('registrationForm')
 myForm.addEventListener('submit', submitRegistration)
-function submitRegistration() {
-    form.reset();
+async function submitRegistration(e) {
+    e.preventDefault()
+    try {
+        let form = new FormData(myForm)
+
+        const response = await fetch('http://localhost:5000/cadastrar', {
+            method: 'POST',
+            body: form
+        })
+
+    } catch (err) {
+        console.error(err)
+    }
+
+    myForm.reset();
     closeModal();
 }
