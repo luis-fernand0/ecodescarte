26af079c15a3ab88583a0a9d7f9a9347be22cc10
luis-fernando-job <fernandoluis270@gmail.com>
2025-10-21 16:49:29 -0400

    tratando exceções e exibindo resposta

--- assets/js/services/findCenters.js
+++ assets/js/services/findCenters.js
@@ -3,39 +3,62 @@ let btnFind = document.getElementById('btn-find-centers')
 btnFind.addEventListener('click', findCenters)
 
 async function findCenters() {
-    const inputFinder = document.getElementById('input-finder').value;
-    const results = document.getElementById('results');
+    try {
+        showLoading()
 
-    if (inputFinder.length === 9) {
-        const response = await fetch(`http://localhost:5000/users?cep=${inputFinder}`)
-        const datas = await response.json()
-        
-        results.innerHTML = 
+        const inputFinder = document.getElementById('input-finder').value;
+        const results = document.getElementById('results');
+
+        if (inputFinder.length < 9) {
+            alert('Por favor, digite um CEP válido (pelo menos 8 dígitos)');
+            return
+        }
+
+        const response = await fetch(`http://localhost:5000/pontos?cep=${inputFinder}`)
+        const data = await response.json()
+
+        if (!response.ok) {
+            results.innerHTML = ``
+            results.classList.remove('show');
+            throw new Error(data.message)
+        }
+
+        if (data.data.length === 0) {
+            results.innerHTML = ``
+            results.classList.remove('show');
+            throw new Error(data.message);
+        }
+
+        results.innerHTML =
             `<h3 class="results-title">
                 <img src="/assets/images/location-icon.png" alt="localização icon"> Centros de Reciclagem Próximos:
             </h3>`
 
-        for (centro in datas) {
-            let centroAtual = datas[centro]
+        for (centro in data.data) {
+            let centroAtual = data.data[centro]
 
             results.innerHTML +=
                 `<div class="result-item">
-                    <div class="result-dot"></div>
-                    <div>
-                        <div class="result-name">
-                            <h4>${centroAtual.nome}</h5>
+                        <div class="result-dot"></div>
+                        <div>
+                            <div class="result-name">
+                                <h4>${centroAtual.nome}</h5>
+                            </div>
+                            <div class="result-info">
+                                <p>Cidade: ${centroAtual.cidade}</p>
+                                <p>Endereço: ${centroAtual.endereco}</p>
+                                <p>N°: ${centroAtual.numero}</p>
+                            </div>
                         </div>
-                        <div class="result-info">
-                            <p>Endereço: ${centroAtual.endereco}</p>
-                            <p>Funcionamento: ${centroAtual?.funcionamento}</p>
-                        </div>
-                    </div>
-                </div>`
+                    </div>`
         }
 
         results.classList.add('show');
-    } else {
-        alert('Por favor, digite um CEP válido (pelo menos 8 dígitos)');
+    } catch (err) {
+        showResponse(err.message)
+        console.error(err.message)
+    } finally {
+        closeLoading()
     }
 }
 
