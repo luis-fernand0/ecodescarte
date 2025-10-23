12e691b1895bc363c3c6e7475359717b99cfd828
luis-fernando-job <fernandoluis270@gmail.com>
2025-10-23 10:46:14 -0400

    exibindo modal de loading e response

--- assets/js/services/cadastrarPonto.js
+++ assets/js/services/cadastrarPonto.js
@@ -2,6 +2,7 @@ let myForm = document.getElementById('registrationForm')
 myForm.addEventListener('submit', submitRegistration)
 async function submitRegistration(e) {
     e.preventDefault()
+    showLoading()
     try {
         let form = new FormData(myForm)
 
@@ -9,11 +10,20 @@ async function submitRegistration(e) {
             method: 'POST',
             body: form
         })
+        
+        let data = await response.json()
+        if (!response.ok) {
+            showResponse(data.message)
+            return
+        }
 
+        showResponse(data.message)
+        myForm.reset();
+        closeModal();
     } catch (err) {
+        showResponse(err.message)
         console.error(err)
+    } finally {
+        closeLoading()
     }
-
-    myForm.reset();
-    closeModal();
 }
