6650281d57cbd847eb5621990414c8e64ebe5819
luis-fernando-job <fernandoluis270@gmail.com>
2025-11-04 11:10:56 -0400

    criando função para validar email via regex

--- assets/js/utils/validarEmail.js
+++ assets/js/utils/validarEmail.js
@@ -1,10 +1,22 @@
 let inputEmail = document.getElementById('email')
 
 inputEmail.addEventListener('input', formatarEmail)
+inputEmail.addEventListener('blur', validarEmail)
 
 function formatarEmail(e) {
     let email = e.target
     email.value = email.value.toLowerCase()
 
     return email
+}
+
+function validarEmail(e) {
+    let email = (e.target.value)
+    let emailRegex = /^((?!\.)[\w\-_.]*[a-zA-Z0-9])(@\w+)(\.\w+(\.\w+)?[^.\W])$/
+    
+    if(!emailRegex.test(email)) {
+        return false
+    }
+
+    return true
 }
